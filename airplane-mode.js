2835e9bfcd8870447843706f563bd54835635248
Damien Soulard <damien.soulard@gmail.com>
2018-10-11 15:51:12 +0200

    feat: hide fb stories

--- airplane-mode.js
+++ airplane-mode.js
@@ -1,4 +1,4 @@
-const elementsToRemove = "[id^=topnews_main_stream]";
+const elementsToRemove = "#stories_pagelet_rhc," + "[id^=topnews_main_stream]";
 
 const removeNode = node => node.parentNode.removeChild(node);
 
