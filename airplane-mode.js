96210ceea355d65567e5cb439bc8997aadab24c1
Damien Soulard <damien.soulard@gmail.com>
2018-10-12 11:03:12 +0200

    fix: js error syntax

--- airplane-mode.js
+++ airplane-mode.js
@@ -2,7 +2,7 @@ const elementsToRemove =
   "#stories_pagelet_rhc," +
   "#pagelet_reminders" +
   ".jewelCount" +
-  "#pagelet_ego_pane",
+  "#pagelet_ego_pane" +
   "[id^=topnews_main_stream]";
 
 const removeNode = node => node.parentNode.removeChild(node);
